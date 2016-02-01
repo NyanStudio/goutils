bb3c14156788f25d31ab130b6264cb46a36ef3b7
Dennis <dennis@ptc-nec.com.tw>
2016-02-01 11:44:08 +0800

    add func Reverse

--- stringutils.go
+++ stringutils.go
@@ -48,3 +48,11 @@ func (su StringUtils) LeftPad(s string, size int, pad string) string {
 		return buffer.String() + s
 	}
 }
+
+func (su StringUtils) Reverse(str string) string {
+    r := []rune(str)
+    for i, j := 0, len(r)-1; i < len(r)/2; i, j = i+1, j-1 {
+        r[i], r[j] = r[j], r[i]
+    }
+    return string(r)
+}
