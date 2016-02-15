766c365844693d455497a1b5760b2d6168267099
Dennis <dennis@ptc-nec.com.tw>
2016-02-15 16:23:09 +0800

    add func RightPad

--- stringutils.go
+++ stringutils.go
@@ -56,3 +56,40 @@ func (su StringUtils) Reverse(str string) string {
     }
     return string(r)
 }
+
+func (su StringUtils) RightPad(s string, size int, pad string) string {
+    if s == "" {
+        return s
+    }
+
+    if pad == "" {
+        pad = SPACE
+    }
+
+    padLen := len(pad)
+    sLen := len(s)
+    pads := size - sLen
+
+    if pads <= 0 {
+        // returns original String when possible
+        return s
+    }
+    if padLen == 1 && pads <= PadLimit {
+        return s + strings.Repeat(pad, pads)
+    }
+
+    if pads == padLen {
+        return s + pad
+    } else if pads < padLen {
+        return s + pad[0:pads]
+    } else {
+        var buffer bytes.Buffer
+
+        for i := 0; i < pads; i++ {
+            idx := i % padLen
+            buffer.WriteString(pad[idx:idx + 1])
+        }
+
+        return s + buffer.String()
+    }
+}
